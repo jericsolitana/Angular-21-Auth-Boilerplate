a75bd19964075837ec146fe47ef62574633774eb
unknown <jericsolitana>
2026-05-22 02:10:06 +0800

    update komit

--- src/app/admin/accounts/add-edit.component.ts
+++ src/app/admin/accounts/add-edit.component.ts
@@ -1,135 +1,85 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { finalize, first } from 'rxjs/operators';
-
-import { AccountService, AlertService } from '@app/_services';
-import { MustMatch } from '@app/_helpers';
-
-@Component({ templateUrl: 'add-edit.component.html', standalone: false })
-export class AddEditComponent implements OnInit, OnDestroy {
-    form!: FormGroup;
-    id?: string;
-    title!: string;
-    Loading = false;
-    submitting = false;
-    submitted = false;
-
-    private loadTimeoutId?: number;
-
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private accountService: AccountService,
-        private alertService: AlertService,
-        private cdr: ChangeDetectorRef
-    ) { }
-
-    ngOnInit() {
-        this.id = this.route.snapshot.params['id'];
-
-        this.form = this.formBuilder.group({
-            title: ['', Validators.required],
-            firstName: ['', Validators.required],
-            lastName: ['', Validators.required],
-            email: ['', [Validators.required, Validators.email]],
-            role: ['', Validators.required],
-            // password only required in add mode
-            password: ['', [Validators.minLength(6), ... (!this.id ? [Validators.required] : [])]],
-            confirmPassword: ['']
-        },  {
-
-            validator: MustMatch('password', 'confirmPassword')
-        });
-
-        this.title = 'Create Account';
-        if (this.id) {
-            this.title = 'Edit Account';
-            this.Loading = true;
-            this.cdr.detectChanges();
-
-            this.loadTimeoutId = window.setTimeout( () => {
-                if (this.Loading) {
-                    this.Loading = false;
-                    this.alertService.error('Request timed out');
-                    this.cdr.detectChanges();
-                }
-            }, 10000);
-
-            this.accountService.getById(this.id)
-                .pipe(
-                    first(),
-                        finalize(() => {
-                            this.Loading = false;
-                            if (this.loadTimeoutId) {
-                                window.clearTimeout(this.loadTimeoutId);
-                                this.loadTimeoutId = undefined;
-                            }
-                            this.cdr.detectChanges();
-                        })
-                )
-
-                .subscribe({
-                    next: x => {
-                        this.form.patchValue(x);
-                        this.cdr.detectChanges();
-                    },
-                    error: error => {
-                        this.alertService.error(error);
-                        this.cdr.detectChanges();
-                    }
-                });
-        }
-    }
-
-    ngOnDestroy() {
-        if (this. loadTimeoutId) {
-            window.clearTimeout(this.loadTimeoutId);
-            this.loadTimeoutId = undefined;
-        }
-    }
-
-    // convenience getter for easy access to form fields
-    get f() { return this.form.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-        this.cdr.detectChanges();
-
-        this.alertService.clear();
-
-        if (this.form.invalid) {
-            return;
-        }
-
-        this.submitting = true;
-        this.cdr.detectChanges();
-
-        // create or update account based on id param
-        let saveAccount;
-        let message: string;
-        if (this.id) {
-            saveAccount = () => this.accountService.update(this.id!, this.form.value);
-            message = 'Account updated';
-        } else {
-            saveAccount = () => this. accountService.create(this.form.value);
-            message = 'Account created';
-
-        }
-
-        saveAccount()
-            .pipe(first())
-            .subscribe({
-                next: () => {
-                    this.alertService.success(message, { keepAfterRouteChange: true });
-                    this.router.navigateByUrl('/admin/accounts');
-                },
-                error: error => {
-                    this.alertService.error(error);
-                    this.submitting = false;
-                    this.cdr.detectChanges();
-                }
-            });
-    }
-}
+<h1>{{title}}</h1>
+<form *ngIf="!Loading" [formGroup]="form" (ngSubmit)="onSubmit()">
+    <div class="row">
+        <div class="mb-3 col-2">
+            <label class="form-label">Title</label>
+            <select formControlName="title" class="form-select" [ngClass]="{ 'is-invalid': submitted && f.title.errors }">
+                <option value=""></option>
+                <option value="Mr">Mr</option>
+                <option value="Mrs">Mrs</option>
+                <option value="Miss">Miss</option>
+                <option value="Ms">Ms</option>
+            </select>
+            <div *ngIf="submitted && f.title.errors" class="invalid-feedback">
+                <div *ngIf="f.title.errors.required">Title is required</div>
+            </div>
+        </div>
+        <div class="mb-3 col-5">
+            <label class="form-label">First Name</label>
+            <input type="text" formControlName="firstName" class="form-control" [ngClass]="{ 'is-invalid': submitted && f.firstName.errors }" />
+            <div *ngIf="submitted && f.firstName.errors" class="invalid-feedback">
+                <div *ngIf="f.firstName.errors.required">First Name is required</div>
+            </div>
+        </div>
+        <div class="mb-3 col-5">
+            <label class="form-label">Last Name</label>
+            <input type="text" formControlName="lastName" class="form-control" [ngClass]="{ 'is-invalid': submitted && f.lastName.errors }" />
+            <div *ngIf="submitted && f.lastName.errors" class="invalid-feedback">
+                <div *ngIf="f.lastName.errors.required">Last Name is required</div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="mb-3 col-7">
+            <label class="form-label">Email</label>
+            <input type="text" formControlName="email" class="form-control" [ngClass]="{ 'is-invalid': submitted && f.email.errors }" />
+            <div *ngIf="submitted && f.email.errors" class="invalid-feedback">
+                <div *ngIf="f.email.errors.required">Email is required</div>
+                <div *ngIf="f.email.errors.email">Email must be a valid email address</div>
+            </div>
+        </div>
+        <div class="mb-3 col-5">
+            <label class="form-label">Role</label>
+            <select formControlName="role" class="form-select" [ngClass]="{ 'is-invalid': submitted && f.role.errors }">
+                <option value=""></option>
+                <option value="User">User</option>
+                <option value="Admin">Admin</option>
+            </select>
+            <div *ngIf="submitted && f.role.errors" class="invalid-feedback">
+                <div *ngIf="f.role.errors.required">Role is required</div>
+            </div>
+        </div>
+    </div>
+    <div *ngIf="id">
+        <h3 class="pt-3">Change Password</h3>
+        <p>Leave blank to keep the same password</p>
+    </div>
+    <div class="row">
+        <div class="mb-3 col">
+            <label class="form-label">Password</label>
+            <input type="password" formControlName="password" class="form-control" [ngClass]="{ 'is-invalid': submitted && f.password.errors }" />
+            <div *ngIf="submitted && f.password.errors" class="invalid-feedback">
+                <div *ngIf="f.password.errors.required">Password is required</div>
+                <div *ngIf="f.password.errors.minlength">Password must be at least 6 characters</div>
+            </div>
+        </div>
+        <div class="mb-3 col">
+            <label class="form-label">Confirm Password</label>
+            <input type="password" formControlName="confirmPassword" class="form-control" [ngClass]="{ 'is-invalid': submitted && f.confirmPassword.errors }" />
+            <div *ngIf="submitted && f.confirmPassword.errors" class="invalid-feedback">
+                <div *ngIf="f.confirmPassword.errors.required">Confirm Password is required</div>
+                <div *ngIf="f.confirmPassword.errors.mustMatch">Passwords must match</div>
+            </div>
+        </div>
+    </div>
+    <div class="mb-3">
+        <button [disabled]="submitting" class="btn btn-primary">
+            <span *ngIf="submitting" class="spinner-border spinner-border-sm me-1"></span>
+            Save
+        </button>
+        <a routerLink="/admin/accounts" class="btn btn-link">Cancel</a>
+    </div>
+</form>
+<div *ngIf="Loading" class="text-center m-5">
+    <span class="spinner-border spinner-border-lg align-center"></span>
+</div>
